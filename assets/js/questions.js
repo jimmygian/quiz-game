e61fca7d0c921a12daff9d74dca5b500713795cb
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2023-11-05 19:39:21 +0000

    Adds questions

---
+++ assets/js/questions.js
@@ -0,0 +1,102 @@
+const questions = [
+    {
+        question: "What is the DOM?",
+        answers: [
+            'The Document Oval Model',
+            'The Don\'t Open Me framework',
+            'Nothing',
+            'The Document Object Model'
+        ],
+        correctAnswerIndex: 3
+    },
+    {
+        question: "How do you select a HTML element in JavaScript?",
+        answers: [
+            'You are not supposed to',
+            'By clicking on the button',
+            'By adding a querySelectorAll',
+            'By adding a querySelector'
+        ],
+        correctAnswerIndex: 3
+    },
+    {
+        question: "What is an event in JavaScript?",
+        answers: [
+            'A type of HTML tag',
+            'A function that logs messages to the console',
+            'A user action that triggers JavaScript code',
+            'A comment in the code'
+        ],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "How can you add an event listener to an HTML element in JavaScript?",
+        answers: [
+            'By using the "style" attribute',
+            'By using the "class" attribute',
+            'By using the "addEventListener" method',
+            'By using the "innerHTML" property'
+        ],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "Which method is used to remove an event listener in JavaScript?",
+        answers: [
+            'removeEvent',
+            'removeListener',
+            'removeEventListener',
+            'off'
+        ],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "What does the 'event.preventDefault()' method do in JavaScript?",
+        answers: [
+            'Stops the event from bubbling up the DOM tree',
+            'Prevents the default browser behavior for the event',
+            'Stops the event propagation',
+            'Resets the event to its initial state'
+        ],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "What is the purpose of the 'event.stopPropagation()' method in JavaScript?",
+        answers: [
+            'Stops the event from bubbling up the DOM tree',
+            'Prevents the default browser behavior for the event',
+            'Stops the event propagation',
+            'Resets the event to its initial state'
+        ],
+        correctAnswerIndex: 0
+    },
+    {
+        question: "What is event delegation in JavaScript?",
+        answers: [
+            'A way to create new events',
+            'A method to remove event listeners',
+            'A technique of handling events on a parent element for multiple child elements',
+            'A way to trigger events programmatically'
+        ],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "Which event is triggered when a user clicks on an HTML element?",
+        answers: [
+            'mouseover',
+            'click',
+            'keydown',
+            'submit'
+        ],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "What is the 'this' keyword in the context of event handling in JavaScript?",
+        answers: [
+            'Refers to the current HTML document',
+            'Refers to the event object',
+            'Refers to the JavaScript file itself',
+            'Refers to the HTML element that triggered the event'
+        ],
+        correctAnswerIndex: 3
+    }
+];
