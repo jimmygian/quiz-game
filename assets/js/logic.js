30c96f65ae47852fdbb4b5c19995f1f5e24ed340
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2023-11-05 21:38:48 +0000

    Correctly checks if question was correct

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,7 +2,7 @@
 
 const startQuizButton = document.querySelector('#start');
 const endScreenDiv = document.querySelector('#end-screen');
-const questionScreenDiv = document.querySelector('#question-screen')
+const questionsDiv = document.querySelector('#questions')
 const feedbackDiv = document.querySelector('#feedback');
 const timeSpan = document.querySelector('#time');
 
@@ -32,6 +32,53 @@ function updateElementVisibility() {
 // Generates Question
 function generateQuestion() {
     console.log('I will generate a question');
+
+    // Selectors
+    const questionText = questionsDiv.querySelector('h2');
+    console.log(questionText);
+    const choices = document.querySelector('#choices');
+    console.log(choices);
+
+    // index
+    index = 0;
+
+    // Update question
+    question = questions[index];
+    questionText.innerText = question.question;
+    
+    // Create buttons
+    for (let i = 0; i < question.answers.length; i++) {
+        // Create New Choice Button
+        const choiceBtn = document.createElement('button');
+
+        // Set attributes
+        choiceBtn.setAttribute('type','submit');
+        choiceBtn.setAttribute('class','questionBtn');
+        choiceBtn.setAttribute('name', i);
+        choiceBtn.innerText = `${i+1}. ` + question.answers[i];
+        
+        // Append to 'choices' div
+        choices.append(choiceBtn);
+    }
+
+    choices.addEventListener('click', function(e){
+        console.log(e.target);
+        clickedBtn = e.target;
+
+        // Runs only if one of the buttons were clicked and not just the parentDiv
+        if (clickedBtn.tagName === 'BUTTON') {
+            const selectedAnswerIndex = parseInt(clickedBtn.getAttribute('name'));
+
+            if (selectedAnswerIndex === question.correctAnswerIndex) {
+                console.log("CORRECT!!!");
+                // Update feedback
+            } else {
+                console.log("WRONG!!!");
+                // Remove time from timer
+                // Update feedback
+            }
+        }
+    });
 }
 
 
@@ -43,7 +90,7 @@ startQuizButton.addEventListener('click', function(e) {
     // Set custon property data-state to "hide"
     const parentDiv = startQuizButton.parentElement;
     parentDiv.dataset.state = "hide";
-    questionScreenDiv.dataset.state = "show";
+    questionsDiv.dataset.state = "show";
     updateElementVisibility();
     generateQuestion();
 });
