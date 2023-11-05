4902e13db28e11f27ee03b8caed359775bc98432
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2023-11-05 19:53:20 +0000

    Adds data-state custom property to html

---
+++ assets/js/logic.js
@@ -0,0 +1,11 @@
+// Create Selectors
+const startQuizButton = document.querySelector('#start');
+const questionsDiv = document.querySelector('#questions');
+const endScreenDiv = document.querySelector('#end-screen');
+const feedbackDiv = document.querySelector('#feedback');
+const timeSpan = document.querySelector('#time');
+
+console.log(startQuizButton);
+console.log(questionsDiv);
+console.log(endScreenDiv);
+console.log(feedbackDiv);
